83c0b7d962a7df7c4242ff0cf3b27ce12a39c6af
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-05-03 22:16:13 +0530

    Updates

---
+++ precache-manifest.98db0d906c572997d5d9fb51fddd13e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d196997044eddac3c55ff26a0a4fcbae",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "e8db7d2ed4a8f68d0ea2",
+    "url": "/tic-tac-toe/static/css/main.4ec3bf7d.chunk.css"
+  },
+  {
+    "revision": "0ea0eb82be11f3a9dc6b",
+    "url": "/tic-tac-toe/static/js/2.faa88d88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.faa88d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8db7d2ed4a8f68d0ea2",
+    "url": "/tic-tac-toe/static/js/main.0bff7825.chunk.js"
+  },
+  {
+    "revision": "5170865a474646a0932d",
+    "url": "/tic-tac-toe/static/js/runtime-main.f9bddb7d.js"
+  },
+  {
+    "revision": "e0c92335158a535bb0053d1c9c96c28f",
+    "url": "/tic-tac-toe/static/media/aq.e0c92335.jpg"
+  }
+]);
